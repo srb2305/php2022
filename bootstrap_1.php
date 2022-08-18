179ec72651acec318a74a05836998ba706d41370
Saurabh Sahu <srb.com2112@gmail.com>
2022-08-18 11:12:03 +0530

    bootstrap table row clm container

---
+++ bootstrap_1.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div class="jumbotron text-center">
+		<h1>My First Bootstrap Page</h1>
+		<p>Resize this responsive page to see the effect!</p>
+	</div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 text-center">
+			<h2>What is Lorem Ipsum?</h2>
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</div>
+		<div class="col-md-4 text-center">
+			<h2>Why do we use it?</h2>
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+		</div>
+		<div class="col-md-4 text-center">
+			<h2>Why do we use it?</h2>
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+		</div>
+	</div>
+
+	<input type="submit" class="btn btn-success">
+	<input type="submit" class="btn btn-info">
+	<input type="submit" class="btn btn-danger">
+	<input type="submit" class="btn btn-warning">
+	<input type="submit" class="btn btn-default">
+
+
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table table-striped table-bordered table-hover" >
+				<thead>
+					<th>Name</th>
+					<th>Email</th>
+					<th>Contact</th>
+					<th>Action</th>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Ram</td>
+						<td>Ram@gmail.com</td>
+						<td>787878787</td>
+						<td>Edit</td>
+					</tr>
+					<tr>
+						<td>Ram</td>
+						<td>Ram@gmail.com</td>
+						<td>787878787</td>
+						<td>Edit</td>
+					</tr>
+					<tr>
+						<td>Ram</td>
+						<td>Ram@gmail.com</td>
+						<td>787878787</td>
+						<td>Edit</td>
+					</tr>
+					<tr>
+						<td>Ram</td>
+						<td>Ram@gmail.com</td>
+						<td>787878787</td>
+						<td>Edit</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+
+</div>
+
+
+</body>
+</html>
